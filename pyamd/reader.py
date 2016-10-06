5064f5369f8de0af3ad46a8e5367f6b995393f38
Shashidhar Ravishankar <shashidhar.ravishankar@gatech.edu>
2016-10-05 23:41:14 -0400

    Full fledged VCF parser added

--- pyamd/reader.py
+++ pyamd/reader.py
@@ -1,6 +1,8 @@
+import re
 import os
 import sys
 import csv
+import vcf
 from collections import namedtuple
 from collections import OrderedDict
 from operator import attrgetter
@@ -47,7 +49,6 @@ class Reader:
         Bed = namedtuple('Bed', ['chrom','start','stop','gene','score', 'strand'])
         for lines in bed_handle:
             line = lines.strip()
-            print(line.split('\t'))
             chrom = line.split('\t')[0]
             start = int(line.split('\t')[1])
             stop = int(line.split('\t')[2])
@@ -66,7 +67,7 @@ class Reader:
         Fasta = namedtuple('Fasta',['header','seq','fid','length'])
         extract = OrderedDict()
         for lines in fasta:
-            extract[lines.header] = lines
+            extract[lines.header] = lines.seq
         return(extract)
 
     def getGenes(self, fasta_file):
@@ -88,63 +89,160 @@ class Reader:
         output.close()
         return
 
-    def readVcf(self, vcf_path):
-        vcf_file = open(vcf_path, 'rt')
-        Vcf = namedtuple('Vcf', ['chrom', 'pos', 'id', 'ref',
-                'alt', 'qual', 'filter', 'info', 'sample'])
-        for lines in vcf_file:
-            if lines[0] == '#':
-                continue
-            var = lines.strip().split('\t')
-            chrom = var[0]
-            pos = int(var[1])
-            ids = var[2]
-            ref = var[3]
-            alt = var[4]
-            qual = float(var[5])
-            filters = var[6]
-            info = var[7]
-            sample = var[8]
-            record = Vcf(chrom, pos, ids, ref, alt, qual, filters,
-                        info, sample)
-            yield record
 
-    def extractVars(self, vcf_path, fasta_path):
-        vcf = self.readVcf(vcf_path)
-        Var = namedtuple('Var', ['chrom', 'pos', 'ref', 'alt', 'codonPos','refCodon', 'altCodon'])
-        fasta = {rec.header : rec.seq for rec in self.readFasta(fasta_path)}
-        for records in vcf:
-            if (records.pos) % 3 == 0:
-                codon_pos = (records.pos)/3
-                codon = fasta[records.chrom][(records.pos-3):records.pos]#[(records.pos-2):records.pos+1]
-                print(records, codon)
-                change = codon[:2] + records.alt 
-                record = Var(records.chrom, records.pos, records.ref, records.alt, codon_pos, codon, change)
-                yield record
-            elif (records.pos +1 ) % 3 == 0:
-                codon_pos = (records.pos +1)/3
-                codon = fasta[records.chrom][(records.pos-2):(records.pos+1)]#[records.pos-1:records.pos+2]
-                print(codon)
-                change = codon[0] + records.alt + codon[2]
-                record = Var(records.chrom, records.pos, records.ref, records.alt, codon_pos, codon, change)
-                yield record
-            elif (records.pos +2) % 3 == 0:
-                codon_pos = (records.pos +2)/3
-                codon = fasta[records.chrom][(records.pos-1):(records.pos+2)]#[records.pos:records.pos+3]
-                print(codon)
-                change = records.alt + codon[1:]
-                record = Var(records.chrom, records.pos, records.ref, records.alt, codon_pos,codon, change)
-                yield record
+    def getContigIDs(self, rec):
+        '''Given a contig header record, return parsed contig object'''
+        rec = re.split('##|=|<|,|>', rec)
+        Contig = namedtuple('Contig', ['id', 'length'])
+        contig = Contig(rec[4], int(rec[6]))
+        return(contig)
+
+
+    def getInfoIDs(self, rec):
+        '''Given a INFO header records, return parsed INFO object'''
+        rec = re.split('##|=|<|,|>|\"', rec)
+        Info = namedtuple('Info', ['id', 'number', 'types', 'desc'])
+        info = Info(rec[4], rec[6], rec[8], rec[11])
+        return(info)
+
+    def getFormatIDs(self, rec):
+        '''Given a FORMAT header record, return parsed FORMAT object'''
+        rec = re.split('##|=|<|,|>|\"', rec)
+        Format = namedtuple('Format', ['id', 'number', 'types', 'desc'])
+        formats = Format(rec[4], rec[6], rec[8], rec[11])
+        return(formats)
+    
+    def getSamples(self, rec):
+        '''Given the vcf header, return the number of samples and list of names'''
+        rec = re.split('#|\t', rec)
+        samples = rec[10:]
+        number = len(samples)
+        Sample = namedtuple('Sample', ['count', 'samples'])
+        sample = Sample(number, samples)
+        return(sample)
+
+    def getRecInfo(self, info, info_dict):
+        rec = re.split(';', info)
+        info_val = {val.split('=')[0]: val.split('=')[1] for val in rec}
+        info_keys = info_dict.keys()
+        Info = namedtuple('Info', info_keys)
+        values = list()
+        for vals in info_dict:
+            try:
+                vals_id = vals
+                vals_value = info_val[vals]
+                if info_dict[vals_id].number == '4':
+                    values.append([int(allele) for allele in vals_value.split(',')])
+                elif info_dict[vals_id].types == 'Integer':
+                    values.append(int(vals_value))
+                elif info_dict[vals_id].types == 'Float':
+                    values.append(float(vals_value))
+                elif info_dict[vals_id].types == 'Flag':
+                    values.append(bool(vals_value))
+                else:
+                    values.append(str(vals_value))
+            except KeyError:
+                values.append(None)
+
+        info = Info(*values)
+        return(info)
+
+    def getRecFormat(self, formats, format_dict, format_order):
+        rec = re.split(':', formats)
+        order = re.split(':', format_order)
+        format_val = {keys : val for keys, val in zip(order,rec)}
+        Format = namedtuple('Format', format_dict.keys())
+        values = list()
+        for vals in format_dict:
+            try:
+                vals_id = vals
+                vals_value = format_val[vals]
+                if format_dict[vals_id].number == 'G':
+                    values.append(vals_value)
+                elif format_dict[vals_id].number == 'R':
+                    values.append([int(allele) for allele in vals_value.split(',')])
+                elif format_dict[vals_id].number == '4':
+                    values.append([int(allele) for allele in vals_value.split(',')])
+                elif format_dict[vals_id].types == 'Integer':
+                    values.append(int(vals_value))
+                elif format_dict[vals_id].types == 'Float':
+                    values.append(float(vals_value))
+                elif format_dict[vals_id].types == 'Flag':
+                    values.append(bool(vals_value))
+                else:
+                    values.append(str(vals_value))
+            except KeyError:
+                values.append(None)
+
+        formats = Format(*values)
+        return(formats)
+
+
+    def readVcf(self, vcf_path):
+        vcf_file = open(vcf_path, 'r')
+        Vcf = namedtuple('Vcf', ['chrom', 'pos', 'id', 'ref',
+                'alt', 'qual', 'filter', 'info','format'])
+        contigs = OrderedDict()
+        info = OrderedDict()
+        formats = OrderedDict()
+        #samples = namedtuple()
+        for lines in vcf_file:
+            lines = lines.strip()
+            if '##' == lines[:2]:
+                if 'contig' == lines[2:8]:
+                    rec_contig = self.getContigIDs(lines)
+                    contigs[rec_contig.id] = rec_contig.length
+                elif 'INFO' == lines[2:6]:
+                    rec_info = self.getInfoIDs(lines)
+                    info[rec_info.id] = rec_info
+                elif 'FORMAT' == lines[2:8]:
+                    rec_format = self.getFormatIDs(lines)
+                    formats[rec_format.id] = rec_format
+
+            elif '#' == lines[0]:
+                samples = self.getSamples(lines)
+            
+            elif '#' != lines[0]:
+                var = lines.strip().split('\t')
+                chrom = var[0]
+                pos = int(var[1])
+                ids = str(var[2])
+                ref = var[3].split(',')[0]
+                alt = var[4].split(',')[0]
+                filters = var[6]
+                qual = float(var[5])
+                infos = self.getRecInfo(var[7], info) 
+                sample = OrderedDict()
+                for sam, form in zip(samples.samples, var[9:]):
+                    sample[sam] = self.getRecFormat(form, formats, var[8])
+                record = Vcf(chrom, pos, ids, ref, alt, qual, filters, infos, sample)
+                yield(record)
+
+    def getAA(self, codon):
+        codontable = {'ATA':'I', 'ATC':'I', 'ATT':'I', 'ATG':'M',
+                      'ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T',
+                      'AAC':'N', 'AAT':'N', 'AAA':'K', 'AAG':'K',
+                      'AGC':'S', 'AGT':'S', 'AGA':'R', 'AGG':'R',
+                      'CTA':'L', 'CTC':'L', 'CTG':'L', 'CTT':'L',
+                      'CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P',
+                      'CAC':'H', 'CAT':'H', 'CAA':'Q', 'CAG':'Q',
+                      'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R',
+                      'GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V',
+                      'GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A'}
+        change = records.alt + codon[1:]
+        refAA = self.getAA(codon)
+        altAA = self.getAA(change)
+        record = Var(records.chrom, records.pos, records.ref, 
+                records.alt, codon_pos, codon, change, refAA, altAA,
+                records.depth, records.freq)
+        yield record
 
             
 
 if __name__ == '__main__':
-    fasta = sys.argv[1]
-    bed = ''
+    vcf_path = sys.argv[1]
     output = './'
-    fastafile = Reader(bed, fasta, output)
-    reader = fastafile.readFasta()
-    for lines in sorted(reader, key=attrgetter('minimizer')):
-        print(lines.minimizer)
-
-
+    fastafile = Reader()
+    reader = fastafile.readVcf(vcf_path)
+    for lines in reader:
+        print(lines)

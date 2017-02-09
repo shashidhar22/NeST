ae7c0d831fef6a15b36b1f052390ca79b41b7cde
Shashidhar Ravishankar <shashidhar.ravishankar@gatech.edu>
2017-02-09 10:40:35 -0500

    Writer module

---
+++ pyamd/writer.py
@@ -0,0 +1,20 @@
+import re
+import os
+import sys
+import csv
+import vcf
+from collections import namedtuple
+from collections import OrderedDict
+from operator import attrgetter
+
+class Writer:
+    
+    def __init__(self):
+        return
+
+    def writeFasta(self, fasta_obj, out_path):
+        out_handle = open(out_path, 'w')
+        for lines in fasta_obj:=`=jedi=0, =`=      (self, *_**args*_*, **kwargs) =`=jedi=`=
+            header = '{0}_{1}'.format(lines.header, lines.fastaid)
+        
+

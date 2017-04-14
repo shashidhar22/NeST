f774f72d81268ae96ffe162d666a0e50af28c794
Shashidhar Ravishankar <shashidhar.ravishankar@gatech.edu>
2017-04-14 12:58:30 -0400

    summarize integrated into MaRS pipeline

--- pyamd/summarize.py
+++ pyamd/summarize.py
@@ -5,10 +5,13 @@ import glob
 import math
 import pandas as pd
 import pysam
-from parsers import Fasta
+from pyamd.parsers import Fasta
+from pyamd.annotater2 import Annotate
 import matplotlib
+matplotlib.use('Agg')
 import numpy as np
 import matplotlib.pyplot as plt
+
 import seaborn as sns
 
 class Summary:
@@ -20,7 +23,7 @@ class Summary:
         self.out_path = out_path
 
     def createTable(self):
-        voi = pd.read_excel('./Reportable_SNPs_Report_v2.xlsx', sheetname=1, parse_cols="B:C")
+        voi = pd.read_excel(self.voi, sheetname=1, parse_cols="B:C")
         voi['Ref'] = voi['SNP'].str.split('\d+', expand=True)[0]
         voi['Alt'] = voi['SNP'].str.split('\d+', expand=True)[1]
         voi['Pos'] = voi['SNP'].str.split('[a-zA-Z]',expand=True)[1]
@@ -28,9 +31,10 @@ class Summary:
         voi[['Pos']] = voi[['Pos']].apply(pd.to_numeric)
         voi.set_index(['Gene','Pos'], inplace=True)
         # Read fasta file
-        reader = Fasta('../../ref/mdr.fa')
+        annotater = Annotate(self.out_path)
+        reader = Fasta(self.fasta)
         #Read bed file
-        bed = pd.read_table('../../ref/mdr.bed', header=None, sep='\t', names=['Gene','Start','Stop','Exon','Info','Strand'])
+        bed = pd.read_table(self.bed, header=None, sep='\t', names=['Gene','Start','Stop','Exon','Info','Strand'])
         #Spliting in codons
         bed_iter = iter(bed.iterrows())
         fasta = reader.read()
@@ -57,13 +61,14 @@ class Summary:
                 codon_list = cdna[bases:bases+3] if cdna[bases][2] == '+' else cdna[bases:bases+3][::-1]
                 codon_seq = ''.join([val[0] for val in codon_list])
                 for codon_pos, nuc in enumerate(codon_list):
-                    codon_table.append((records.header, (records.header, nuc[1], nuc[0], codon, codon_seq, getAA(codon_seq), codon_pos)))
+                    codon_table.append((records.header, (records.header, nuc[1], nuc[0], codon, codon_seq,
+                                                         annotater.getAA(codon_seq), codon_pos)))
 
         codon_data = pd.DataFrame.from_items(codon_table, orient='index', columns=['Gene', 'NucPos', 'Nuc', 'Pos', 'Codon', 'AA', 'CodonPos'])
         codon_data[['Pos']] = codon_data[['Pos']].apply(pd.to_numeric)
         codon_data.set_index(['Gene', 'Pos'], inplace=True)
         codons_of_int = voi.merge(codon_data, left_index=True, right_index=True, how='inner' )
-        codons_of_int['AltCodon'] = codons_of_int['Alt'].apply(getCodon)
+        codons_of_int['AltCodon'] = codons_of_int['Alt'].apply(annotater.aaToCodon)
         return(codons_of_int)
 
     def getBamStat(self, bamfile, chrom, pos, codon_pos):
@@ -72,26 +77,31 @@ class Summary:
         if codon_pos == 0:
             avg_codon_coverage = bamfile.count(chrom, pos-1, pos+2)
         elif codon_pos == 1:
-            avg_codon_coverage = bamfile.count_coverage(chrom, pos-2, pos+1)
+            avg_codon_coverage = bamfile.count(chrom, pos-2, pos+1)
         else:
-            avg_codon_coverage = samfile.count_coverage(chrom, pos-3, pos)
+            avg_codon_coverage = bamfile.count(chrom, pos-3, pos)
         return(avg_codon_coverage)
 
     def getVarOfInt(self, vcf_path):
         sample_dirs = glob.glob('{0}/*'.format(out_path))
+        codons_of_int = self.createTable()
         for samples in sample_dirs:
             sample_folder = os.path.basename(samples)
-            sample_bam = '{0}/output_sorted_RG.bam'
-            sample_vcf = vcf.Reader(filename='{0}/{0}_merged_annotated.vcf'.format(samples))
+            sample_bam = '{0}/output_sorted_RG.bam'.format(samples)
+            if not os.path.isdir(samples):
+                continue
+            sample_vcf = vcf.Reader(filename='{0}/{1}_variants_merged_annotated.vcf'.format(samples, sample_folder))
             sample_name = sample_vcf.samples[0]
             sample_result = []
             sample_vars = {'{0}_{1}_{2}_{3}'.format(var.CHROM, var.POS, var.INFO['RefAA'][0], var.INFO['AltAA'][0]): var.INFO['DP'] for var in sample_vcf}
             for val in codons_of_int.iterrows():
                 if '{0}_{1}_{2}_{3}'.format(val[0][0], val[1]['NucPos'], val[1]['Ref'],val[1]['Alt']) in sample_vars.keys():
-                    depth = sample_vars['{0}_{1}_{2}_{3}'.format(val[0][0], val[1]['NucPos'], val[1]['Ref'],val[1]['Alt'])]
+                    depth = math.log2(sample_vars['{0}_{1}_{2}_{3}'.format(val[0][0], val[1]['NucPos'],
+                                                                           val[1]['Ref'],val[1]['Alt'])]+1)
                     sample_result.append(depth)
                 else:
-                    depth = -1 * self.getBamStat(sample_bam, val[0][0], val[1]['NucPos'], val[1]['CodonPos'])
+                    depth = -1 * math.log2(self.getBamStat(sample_bam, val[0][0], val[1]['NucPos'],
+                                                           val[1]['CodonPos'])+1)
                     sample_result.append(depth)
             sample_series = pd.Series(sample_result, index=codons_of_int.index)
             codons_of_int[sample_name] = sample_series
@@ -101,13 +111,14 @@ class Summary:
         heatmap = sns.heatmap(codons_of_int.iloc[:,9:],linewidths=.5, square=False)
         fig = heatmap.get_figure()
         fig.savefig('{0}/heatmap.png'.format(self.out_path))
+        
 
 
 if __name__ == '__main__':
-	fasta_path = sys.argv[1]
+    fasta_path = sys.argv[1]
     bed_path = sys.argv[2]
     voi_path = sys.argv[3]
-	out_path = sys.argv[4]
-	summarizer = Summary(fasta_path, bed_path, voi_path, out_path)
-	summarizer.getVarOfInt(out_path)
+    out_path = sys.argv[4]
+    summarizer = Summary(fasta_path, bed_path, voi_path, out_path)
+    summarizer.getVarOfInt(out_path)
     

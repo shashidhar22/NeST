37d30fccc78de2802f5d64f28f3760b2b78e348d
Shashidhar Ravishankar <shashidhar.ravishankar@gatech.edu>
2017-05-25 11:12:29 -0400

    Fixed pandas warnings

--- pyamd/summarize.py
+++ pyamd/summarize.py
@@ -12,8 +12,10 @@ import matplotlib
 matplotlib.use('Agg')
 import numpy as np
 import matplotlib.pyplot as plt
-
+import warnings
 import seaborn as sns
+
+warnings.filterwarnings('ignore')
 
 class Summary:
 

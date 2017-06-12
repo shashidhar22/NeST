607ff7ee6e2cc1db67903b57f423fcba4f3e8460
Shashidhar Ravishankar <shashidhar.ravishankar@gatech.edu>
2017-06-12 14:26:33 -0400

    Revert "Batch mode run script"
    This reverts commit 4a1c78dd45d808f91e9e711774426007d67ab096.

--- run_batch.sh
+++ run_batch.sh
@@ -1,2 +1,2 @@
-time python3.4 pyamd.py -i /projects/home/sravishankar9/Dropbox/MaRS_data/test -r ref/mdr.fa -a lib/bbmap/resources/adapters.fa -o /projects/home/sravishankar9/Dropbox/MaRS_data/testMaRS -b ref/mdr.bed -m bbmap --varofint ref/Reportable_SNPs_Report_v2.xlsx  
+time python3.4 pyamd.py -i /projects/home/sravishankar9/Dropbox/MaRS_data/160805_M04080_0031_000000000-D251C_KEL5430 -r ref/mdr.fa -a lib/bbmap/resources/adapters.fa -o /projects/home/sravishankar9/Dropbox/MaRS_data/testMaRS -b ref/mdr.bed -m bwa --varofint ref/Reportable_SNPs_Report_v2.xlsx  
 #160805_M04080_0031_000000000-D251C_KEL5430

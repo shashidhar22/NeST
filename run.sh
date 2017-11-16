a036adf6b84ab208e2bf56e201430a21613e957c
Shashidhar Ravishankar <shashidhar.ravishankar@gatech.edu>
2017-11-16 14:20:16 -0500

    Run script for MaRS

--- run.sh
+++ run.sh
@@ -1,9 +1,9 @@
 display_usage(){
-	echo "run.sh <path to read one> <path to read two> <path to output dir>"
+	echo "run.sh <path to experiment folder> <path to output folder>"
 }
 if [ $# -eq 0 ]; then
 	display_usage
 	exit 1
 fi	
  
-python3.4 pyamd.py -1 $1 -2 $2 -a ./lib/bbmap/resources/adapters.fa -r ref/mdr.fa -o $3 -b ./ref/mdr.bed -m bwa
+python3.4 pyamd.py -i $1 -a ref/adapters.fa -r ref/mdr.fa -o $2 -b ref/mdr.bed -m bwa --varofint ref/Reportable_SNPs.xlsx

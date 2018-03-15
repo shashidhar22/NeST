ebb175eb9289d811778b9a60112639c2d1ffd09e
Shashidhar Ravishankar <shashidhar.ravishankar@gatech.edu>
2018-03-15 01:55:29 -0400

    Modified GATK and Picard commands for conda environment

--- pyamd/gatk.py
+++ pyamd/gatk.py
@@ -19,7 +19,7 @@ class GenAnTK:
     def hapCaller(self, bam_path, ref_path, sam_name):
         vcf_path = '{0}/{1}_variants_gatk.vcf'.format(self.out_path, sam_name)
 
-        hcmd = [self.java, '-jar', self.gatk_path, '-T', 'HaplotypeCaller', '-R', ref_path,
+        hcmd = [self.gatk_path, '-T', 'HaplotypeCaller', '-R', ref_path,
             '-I', bam_path, '-o', vcf_path, '-nct', '1', '-sn', sam_name,
             '-gt_mode', 'DISCOVERY']
         hrun = subprocess.Popen(hcmd, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL, shell=False)
@@ -39,7 +39,7 @@ class Picard:
 
     def picard(self, bam_path, sam_name):
         add_path = '{0}/{1}_RG.bam'.format(self.out_path, os.path.splitext(os.path.basename(bam_path))[0])
-        acmd = [self.java, '-Xmx1g', '-jar', self.pic_path, 'AddOrReplaceReadGroups',
+        acmd = [self.pic_path, 'AddOrReplaceReadGroups',
             'I='+bam_path , 'O='+add_path, 'SORT_ORDER=coordinate', 'RGID={0}'.format(sam_name),
             'RGLB=ExomeSeq', 'RGPL=Illumina', 'RGPU=HiSeq2500', 'RGSM={0}'.format(sam_name),
             'RGCN=AtlantaGenomeCenter', 'RGDS=ExomeSeq', 'RGDT=2016-08-24', 'RGPI=null',

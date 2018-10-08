00e9845a8c39a04b22d574576d8ed350e7cb79be
Shashidhar Ravishankar <shashidhar.r.shankar@gmail.com>
2018-10-08 02:00:05 -0400

    Added a check to see if the terminal had a bashrc or bash_profile file

--- install.sh
+++ install.sh
@@ -10,9 +10,17 @@ fi
 # Silent install Miniconda into HOME directory
 bash ./lib/miniconda.sh -b -p $HOME/miniconda
 # Add conda activation script to bashrc
-echo -e "\n#Adding conda activation script to .bashrc \n. $HOME/miniconda/etc/profile.d/conda.sh" >> ~/.bash_profile
-# Refresh terminal
-source ~/.bash_profile
+if [ -f ~/.bash_profile ]; then
+    echo "Appending conda path to bash_profile"
+    echo -e "\n#Adding conda activation script to .bashr_profile \n. $HOME/miniconda/etc/profile.d/conda.sh" >> ~/.bash_profile
+elif [ -f ~/.bashrc ]; then
+    echo "Appending conda path to bashrc"
+    echo -e "\n#Adding conda activation script to .bashrc \n. $HOME/miniconda/etc/profile.d/conda.sh" >> ~/.bashrc
+else
+    echo "Could not find bashrc or bash_profile, please append this to your bashrc or profile files"
+    echo ". $HOME/miniconda/etc/profile.d/conda.sh"
+fi
+
 # Update conda
 ~/miniconda/bin/conda update -y conda
 # Install Anaconda dependencies and packages
